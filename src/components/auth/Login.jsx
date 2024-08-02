ab4ef4820ddfaafda3b5bb5abead91ec8b674afe
Soy Xavier <xavigerardo2002@gmail.com>
2024-08-02 11:40:41 -0600

    Se agrego el olvidar contrasena tanto en el log in como dentro de la app.

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -3,7 +3,7 @@ Este componente funcional es la pantalla principal del Login.
 Sirve para iniciar sesión y redirigir una vez que se haya iniciado sesión correctamente al componente
 funcional del perfil del usuario.
 */
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react"
 import { auth } from "../../config/firebase";
 import { useNavigate, Link } from "react-router-dom";
@@ -11,6 +11,7 @@ import { LoginGoogle } from "./LoginGoogle";
 import { LoginFacebook } from "./LoginFacebook";
 import 'bootswatch/dist/litera/bootstrap.min.css'
 import "../../styles/login.css";
+import Swal from "sweetalert2";
 
 export const Login = () => {
     
@@ -34,6 +35,49 @@ export const Login = () => {
         } catch (error) {
             console.log(error.message);
         }
+    }
+
+    const recoverPassword = ()=>{
+        Swal.fire({
+            title: "Digite su correo electronico",
+            input: "text",
+            inputAttributes: {
+              autocapitalize: "off"
+            },
+            showCancelButton: true,
+            confirmButtonText: "Recuperar contrasena",
+            showLoaderOnConfirm: true,
+            preConfirm: async (username) => {
+            
+                
+            const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            
+            if (!emailPattern.test(username)) {
+                Swal.showValidationMessage("Debe digitar un correo electrónico válido");
+                return;
+            }
+
+            try {
+            
+                await sendPasswordResetEmail (auth, username)
+                .then(()=>{
+                  Swal.fire({
+                    title: "Email enviado",
+                    text: "Se ha enviado un correo para cambiar su contraseña",
+                    icon: "success"
+                  });
+                })
+                
+              } catch (error) {
+                Swal.fire({
+                    title: "Error",
+                    text: "Debe digitar un correo electronico valido",
+                    icon: "error"
+                  });
+              }
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+          })
     }
 
     return (
@@ -78,6 +122,14 @@ export const Login = () => {
                 <LoginGoogle />
                 <br />
                 <LoginFacebook />
+                <hr className="mt-3"/>
+                <label className="mt-3" style={{textAlign:'center'}}>Olvido su contraseña?</label>
+                <div >
+                    <button type="button" onClick={recoverPassword} className="btn btn-secondary">
+                    Recuperar contrasena
+                    </button>
+                </div>
+               
                 </form>
             </div>
         </div>
