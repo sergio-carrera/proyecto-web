d8a26987219cf5705ffe8bb709d2648e0bd2ca7d
Sergio Carrera <sergiocarrerahidalgo@gmail.com>
2024-08-04 21:41:58 -0600

    Merge del feature crear-publicacion

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -20,24 +20,24 @@ Con esta constante se inicializa la "aplicación" Firebase con la configuración
 en el objeto "firebaseConfig".
 */
 const app = initializeApp(firebaseConfig);
+
 /*
 Se obtiene la instancia del servicio de autenticación de Firebase y se exporta para poder usar esta funcionalidad 
 en todos los componentes funcionales respectivos. (con esto podemos contrastar si un usuario está registrado,
 logueado, cuál es el id del usuario, etc.).
 */
- export const auth = getAuth();
+export const auth = getAuth();
 
- 
- /*
+/*
 Se obtiene la instancia del servicio de almacenamiento de Firebase y se exporta para poder usar esta funcionalidad 
 en todos los componentes funcionales respectivos. (con esto podemos subir, descargar y gestionar archivos).
 */
- export const storage = getStorage(app);
+export const storage = getStorage(app);
 
 /*
 Se exporta la base de datos como hemos visto en clase, para poder realizar CRUD con la base de datos.
 */
- export const db = getFirestore(app);
+export const db = getFirestore(app);
  
 //Se exporta la "aplicación" Firebase.
 export default app;
