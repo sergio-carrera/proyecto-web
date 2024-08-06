7db20dd3f8e8e324981d27c140ad501200fac521
Soy Xavier <xavigerardo2002@gmail.com>
2024-08-05 18:23:17 -0600

    Se realiza la desactivacion de cuentas y a su vez que no se pueda loguear si la cuenta esta inactiva

--- src/components/pages/AdminGestionUsuario.jsx
+++ src/components/pages/AdminGestionUsuario.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
-import { onDelete, onFindAll } from "../../config/Api"
-import { auth } from "../../config/firebase"
+import { onFindAll, onFindById, onUpdate } from "../../config/Api"
 import Swal from "sweetalert2"
+
 
 
 
@@ -19,40 +19,28 @@ const AdminGestionUsuario = () => {
         onGetUsuarios(collectionString)
     },[])  
   
-    const eliminarUsuario =  () => {
+    const eliminarUsuario = async   ({target}) => {
         
-        
-        /*if (user) {
-          
-          Swal.fire({
-            title: "Estas seguro?",
-            text: "La cuenta no se podrá recuperar",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Eliminar cuenta!"
-          }).then(async (result) => {
-            if (result.isConfirmed) {
-            await onDelete(collectionString,target.dataset.id)
-            user
-              .delete()
-              .then(() => {
-                onGetUsuarios();
-            })
-            .catch((error) => {
-              console.error("Error deleting user:", error);
-            
-            });
-              Swal.fire({
-                title: "Eliminado!",
-                text: "la cuenta fue eliminada correctamente",
-                icon: "success"
-              });
-            }
-          });
-        } */
-      };
+      const datos = await onFindById(collectionString,target.dataset.id)
+      
+      const objectAux = datos.data()
+
+      
+
+      if (objectAux.Estado == 'Inactivo'){
+        objectAux.Estado = "Activo"
+        Swal.fire("Usuario activado correctamente!");
+      
+      }else{
+        objectAux.Estado = "Inactivo"
+        Swal.fire("Usuario desactivado correctamente!");
+      }
+
+      onUpdate(collectionString, target.dataset.id, objectAux)
+
+      
+      onGetUsuarios(collectionString)
+    };
   
 return (
     <>
@@ -62,6 +50,7 @@ return (
                         <th>Nombre</th>
                         <th>Apellidos</th>
                         <th>Correo</th>
+                        <th>Estado</th>
                         <th>Foto de perfil</th>
             </tr>
         </thead>
@@ -75,11 +64,12 @@ return (
                                 <th>{documento.data().nombre}</th>
                                 <th>{documento.data().apellido}</th>
                                 <th>{documento.data().email}</th>
+                                <th>{documento.data().Estado}</th>
                                 <th><img src={documento.data().foto} style={{height:'75px', width:'80px'}}></img></th>
                                     <th>
                               
                                     <button className="btn btn-primary " style={{width:'200px',height:'65px'}}  data-id={documento.id} >Ver publicaciones del usuario</button>
-                                    <button className="btn btn-danger ms-3" data-id={documento.id} style={{width:'200px',height:'65px'}} onClick={eliminarUsuario} >Eliminar usuario</button>
+                                    <button className={documento.data().Estado=="Inactivo"?'btn btn-success mt-3':'btn btn-danger mt-3'} data-id={documento.id} style={{width:'200px',height:'65px'}} onClick={eliminarUsuario} >{documento.data().Estado=="Inactivo"?'Activar Usuario':'Desactivar usuario'}</button>
                                 </th>
                                 
                             </tr>
