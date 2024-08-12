a307595364f3e674eefc65486780ffba426b7282
Soy Xavier <xavigerardo2002@gmail.com>
2024-08-11 23:28:31 -0600

    se agrega el admin que puede ver y eliminar todas las publicaciones

--- src/components/pages/Admin.jsx
+++ src/components/pages/Admin.jsx
@@ -8,12 +8,15 @@ const goUserAdmin =()=>{
     navigate('/AdminGestionUsuario')
 }
     
+const goAdminPublicaciones =()=>{
+    navigate('/AdminTodasPublicacion')
+}
 return (
     <>
         <div className="m-3" style={{ width:"1920px", textAlign:'center'}}>
             <button className="btn btn-primary mb-2" style={{height:'60px', width:'400px'}} onClick={goUserAdmin}>Gestion de usuarios</button>
             <br />
-            <button className="btn btn-success mb-2" style={{height:'60px', width:'400px'}} >Gestion de publicaciones</button>
+            <button className="btn btn-success mb-2" style={{height:'60px', width:'400px'}} onClick={goAdminPublicaciones}>Gestion de publicaciones</button>
             <br />
             <button className="btn btn-danger mb-2" style={{height:'60px', width:'400px'}}>Gestion de reportes</button>
         </div>
